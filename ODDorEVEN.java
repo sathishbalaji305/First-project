639ae3da53328d2e2424da167b49596031b5c697
sathishbalaji305 <124029161+sathishbalaji305@users.noreply.github.com>
2023-02-02 19:42:39 +0530

    Add files via upload

---
+++ ODDorEVEN.java
@@ -0,0 +1,16 @@
+package com.git;
+
+public class ODDorEVEN {
+
+	public static void main(String[] args) {
+		
+		int a=11;
+		
+		if(a%2==0) {
+			System.out.println(a+"is even number");
+		}
+		else {
+			System.out.println(a+"is odd number");
+		}
+	}
+}
