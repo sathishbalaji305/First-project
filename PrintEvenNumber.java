2369167964caedb74999fdb61efcf5120532c5ac
sathishbalaji305 <124029161+sathishbalaji305@users.noreply.github.com>
2023-02-07 15:12:55 +0530

    Add files via upload

---
+++ PrintEvenNumber.java
@@ -0,0 +1,14 @@
+package com.git;
+
+public class PrintEvenNumber {
+
+	public static void main(String []args) {
+		
+		for (int i=1;i<=60;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+		
+	}
+}
