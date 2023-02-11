357093938f36105d9845373fce91ebe6b41f354a
sathishbalaji305 <124029161+sathishbalaji305@users.noreply.github.com>
2023-02-11 11:10:15 +0530

    Add files via upload

---
+++ SwapNum.java
@@ -0,0 +1,18 @@
+package com.git;
+
+public class SwapNum {
+
+	
+	public static void main(String[]args) {
+		int a=78;
+		int b=98;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		int temp=a;
+		a=b;
+		b=temp;
+		System.out.println("a="+a);
+		System.out.println("b="+b);
+		
+	}
+}
