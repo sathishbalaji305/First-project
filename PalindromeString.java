28fc4cebe58b7925520f5086928a3a258b537e9a
sathishbalaji305 <124029161+sathishbalaji305@users.noreply.github.com>
2023-02-17 11:45:45 +0530

    Add files via upload

---
+++ PalindromeString.java
@@ -0,0 +1,19 @@
+package com.git;
+
+public class PalindromeString {
+
+	public static void main(String[]args) {
+		String s="malayalam";
+		String rev="";
+		for(int i=s.length()-1;i>=0;i--) {
+			rev=rev+s.charAt(i);
+		}
+		if(s.equals(rev)) {
+			System.out.println("Palindrome");
+		}
+		else {
+			System.out.println("Not a Palindrome");
+		}
+	}
+	
+}
